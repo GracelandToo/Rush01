8c3680b53ef15a3825112a06500c35e72ac1ed56
GracelandToo <103772936+GracelandToo@users.noreply.github.com>
2023-02-26 02:36:53 +0100

    add all necessary verifications

--- main.c
+++ main.c
@@ -3,99 +3,140 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
+/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 14:10:37 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/02/25 22:30:54 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/02/25 23:15:24 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
+#include "ft_verify.c"
+#include "ft_print_2d.c"
 
-int	ft_strlen(char *str)
+
+int ft_verify_top(int comb[4][4], int parameters[16], int i, int j)
 {
-	int	length;
-
-	length = 0;
-
-	while (str[length] != '\0')
-	{
-		length++;
-	}
-	return (length);
-}
-
-void ft_print_error(char *error_msg)
-{
-	write(2, "ERROR: ", 7);
-	write(2, error_msg, ft_strlen(error_msg));
-	write(2, "\n", 1);
-}
-
-int main(int argc, char **argv)
-{
-	int		index;
-	int		index_i;
-	int		input[16];
-	int		error;
-	int		rest_length;
-	char	*str_rest;
-	int 	loop;
-
-	index = 0;
-	index_i = 0;
-	error = 0;
-	str_rest = argv[1];
-	rest_length = ft_strlen(str_rest);
-	printf("longitud string rest %d\n", rest_length);
-	printf("contenido string rest %s\n", str_rest);
-	if (argc != 2)
-		error = 1;
-	while ((error == 0) && (index < rest_length) && (str_rest[index] != '\0')) 
-	{
-		if (str_rest[index] == ' ')
-			index++;
-		else if ((str_rest[index] < '1') || (str_rest[index] > '4' ))
-			error = 2;
-		else if ((str_rest[index + 1] != ' ') && (str_rest[index + 1] != '\0'))
-			error = 3;
-		else
-		{
-			input[index_i] = (str_rest[index] - 48);
-			// printf("%d-", input[index_i]);
-			index++;
-			index_i++;
-		}
-	}
-
-	//printf("\n%d\n", index);
-	printf("Contenido de array -> input: ");
-	for (loop = 0; loop < 16; loop++)
-		printf("%d", input[loop]);
+	int row;
+	int height;
+	int visible;
 	
-	if (error == 1)
+	while (++i < 4)
 	{
-		ft_print_error("Only 1 argument required\n");
+		row = 0;
+		visible = 1;
+		height = comb[0][++j];
+		while (++row <= 3)
+		{
+			if(comb[row][j] > height)
+			{
+				height = comb[row][j];
+				visible++;
+			}
+		}
+		if(visible < parameters[i])
+			return 0;
 	}
-	else if (error == 2)
-	{
-		ft_print_error("Incorrect value: Out of range, only number from 1 to 4 admited.\n");
-	}
-	else if (error == 3)
-	{
-		ft_print_error("Incorrect value: Too many or few digits, only 1 digit.\n");
-	}
-	else if (index_i < 15)
-	{
-		//error = 4;
-		ft_print_error("Too few values, 16 required\n");
-	}
-	else if	(index_i > 16)
-	{
-		//error = 5;
-		ft_print_error("Too many values, 16 required\n");
-	}
+	return (1);
+}
 
-	// acá verify y generación de combinanciones
+int ft_verify_down(int comb[4][4], int parameters[16], int i, int col)
+{
+	int row;
+	int height;
+	int visible;
+	
+	while (++i < 8)
+	{
+		row = 4;
+		visible = 1;
+		height = comb[3][++col];
+		while (--row >= 0)
+		{
+			if(comb[row][col] > height)
+			{
+				height = comb[row][col];
+				visible++;
+			}
+		}
+		if(visible < parameters[i])
+			return 0;
+	}
+	return (1);
+}
+
+int ft_verify_left(int comb[4][4], int parameters[16], int i, int row)
+{
+	int column;
+	int height;
+	int visible;
+	
+	while (++i < 12)
+	{
+		column = 0;
+		visible = 1;
+		height = comb[++row][0];
+		while (++column <= 3)
+		{
+			if(comb[row][column] > height)
+			{
+				height = comb[row][column];
+				visible++;
+			}
+		}
+		if(visible < parameters[i])
+			return 0;
+	}
+	return (1);
+}
+
+int ft_verify_right(int comb[4][4], int parameters[16], int i, int row)
+{
+	int column;
+	int height;
+	int visible;
+	
+	while (++i < 16)
+	{
+		column = 4;
+		visible = 1;
+		height = comb[++row][3];
+		while (--column >= 0)
+		{
+			if(comb[row][column] > height)
+			{
+				height = comb[row][column];
+				visible++;
+			}
+		}
+		if(visible < parameters[i])
+			return 0;
+	}
+	return (1);
+}
+
+int main(void)
+{
+	int parameters[16] = {4, 3, 2, 1, 1, 2, 2, 2, 4, 3, 2, 1, 1, 2, 2, 2};
+	int comb[4][4] = {
+    {1, 2, 3, 4},
+    {2, 3, 4, 1},
+	{3, 4, 1, 2},
+	{4, 1, 2, 3}
+	};
+
+	ft_verify_solvability(parameters);
+	printf("%i", ft_verify_rows(comb));
+	printf("%i", ft_verify_columns(comb));
+	ft_print_comb(comb);
+
+	//me quedaba sin espacio por declarar variables
+	//como solucion saqué dos de ellas y las puse como parametros
+	//como para ahorrar espacio estoy operando desde la primera vez que
+	//son visibles en el codigo, las envio como uno menos que el valor que deseo
+	printf("\n%i", ft_verify_top(comb, parameters, -1, -1));
+	printf("\n%i", ft_verify_down(comb, parameters, 3, -1));
+	printf("\n%i", ft_verify_left(comb, parameters, 7, -1));
+	printf("\n%i", ft_verify_right(comb, parameters, 11, -1));
+
 }
